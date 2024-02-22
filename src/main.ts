313ba314e3ca5e3f43ec2e62d2b8bc647f0072b3
Filain <ratecurrent@gmail.com>
2024-02-22 21:12:53 +0200

    add structure

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './modules/app/app.module';
+import { AppModule } from './modules/app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { SwaggerHelper } from './common/helpers/swagger.helper';
 
