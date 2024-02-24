65d7bca0e8e13c134453b958fb3a3fd3e4ad7841
Filain <ratecurrent@gmail.com>
2024-02-24 14:52:45 +0200

    add configs . env docker

--- src/modules/app.module.ts
+++ src/modules/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 
+import configuration from '../configs/configs';
 import { HealthModule } from './health/health.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [UserModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      isGlobal: true,
+    }),
+    UserModule,
+    HealthModule,
+  ],
   controllers: [],
   providers: [],
 })
